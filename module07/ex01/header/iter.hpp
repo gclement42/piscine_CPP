a64fa22a89c8b0633e1bae50d6754d5db34aa4ae
gclement42 <clement.games125@gmail.com>
2023-09-22 11:00:05 +0200

    fini module07

---
+++ module07/ex01/header/iter.hpp
@@ -0,0 +1,22 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template <typename T, typename L>
+void iter(T *arr, L len, void (*f)(T &arg))
+{
+	L i;
+
+	i = 0;
+	while (i < len)
+	{
+		f(arr[i]);
+		i++;
+	}
+}
+
+template <typename T>
+void test(T &arg)
+{
+	arg += 1;
+}
+#endif
