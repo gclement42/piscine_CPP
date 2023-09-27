8b61632284e40869493bb7b6d73c96bcf06a9644
gclement42 <clement.games125@gmail.com>
2023-09-27 16:03:44 +0200

    debut module09

--- module09/ex00/header/BitcoinExchange.hpp
+++ module09/ex00/header/BitcoinExchange.hpp
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gclement <gclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/27 14:21:34 by gclement          #+#    #+#             */
-/*   Updated: 2023/09/27 14:21:45 by gclement         ###   ########.fr       */
+/*   Created: 2023/09/27 13:40:34 by gclement          #+#    #+#             */
+/*   Updated: 2023/09/27 14:57:18 by gclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
 
-int main(int argc, char **argv)
+#include <iostream>
+
+class BitcoinExchange
 {
-	(void)argc;
-	(void)argv;
-	return (0);
+	public :
+		BitcoinExchange();
+		~BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &src);
+		BitcoinExchange &operator=(const BitcoinExchange &src);
+	
+		
 }
+
+#endif
--- module09/ex00/src/BitcoinExchange.cpp
+++ module09/ex00/src/BitcoinExchange.cpp
@@ -1,20 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gclement <gclement@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/27 14:21:34 by gclement          #+#    #+#             */
-/*   Updated: 2023/09/27 14:21:45 by gclement         ###   ########.fr       */
+/*   Created: 2023/09/27 13:41:06 by gclement          #+#    #+#             */
+/*   Updated: 2023/09/27 13:41:13 by gclement         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(int argc, char **argv)
-{
-	(void)argc;
-	(void)argv;
-	return (0);
-}
